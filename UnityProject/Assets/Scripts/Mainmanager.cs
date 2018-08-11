560ed8a453642ad3c25806f4f9fcaeb17ddeff5c
Sheryl <sheryl.galimba@yahoo.com>
2018-08-11 19:57:15 +0800

    ay

---
+++ UnityProject/Assets/Scripts/Mainmanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Mainmanager : MonoBehaviour
+
+
+{
+
+
+
+    ///MAIN FUNCTION
+    public void playbtn()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void exitbtn()
+    {
+        Application.Quit();
+        //EditorApplication.Exit(0);
+    }
+
+
+}
---
+++ UnityProject/Assets/Template/PennyPixel/Scripts/pauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class pauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseUI;
+    //public GameObject continueUI;
+
+
+    //MAIN FUNCTION
+
+    public void pauseBtn()
+    {
+        pauseUI.SetActive(true);
+    }
+
+    public void continueBtn()
+    {
+        pauseUI.SetActive(false);
+    }
+    public void retryBtn()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void exitBtn()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
